f7dca5210935514ea2385f7dc01b0380eb620fb8
Jerry Yuan <yhyuan@gmail.com>
2024-08-31 11:26:45 -0400

    add bitcoin wallet project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,16 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'A Bitcoin Wallet Implementation from Scratch',
+    description: `This project aims to implement Bitcoin-related algorithms from scratch, allowing the creation of a Bitcoin wallet on an embedded device without relying on any third-party libraries.`,
+    imgSrc: '/static/images/bitcoin.png',
+    href: 'https://github.com/yhyuan/bitcoin-rust-lib',
   },
 ]
 
